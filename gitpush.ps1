369f9b1e0cd5b26088f177c1c947ea25f5156e28
Adrian Tischler <a.tischler18596@hs-ansbach.de>
2026-07-08 10:27:29 +0200

    Messung: 07/08/2026 10:27:29

--- gitpush.ps1
+++ gitpush.ps1
@@ -1,11 +1,8 @@
 
-cat argo2.ps1 | powershell
-# 1. Zeitstempel VOR dem Push nehmen
-$start_push = Get-Date
+
 
 # 2. Git Befehle
 git add .
 git commit -m "Messung: $(Get-Date)"
 git push origin main
 
-cat argo2.ps1 | powershell
